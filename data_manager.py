c2afcba158c44bbaf5a80f85680ce81d0fcbc16a
Aliot26 <olgakochanowa@gmail.com>
2019-01-16 10:14:52 +0100

    Added input/output to csv file functionality

--- data_manager.py
+++ data_manager.py
@@ -1,8 +1,19 @@
 import uuid
+import csv
+import connection
+
 
 def get_next_id(list_of_dict):
     new_id = str(uuid.uuid4())[:6]
     for dict in list_of_dict:
         if dict['id'] == new_id:
-            get_id(list_of_dict)
+            get_next_id(list_of_dict)
     return str(new_id)
+
+
+def get_one_question(question_id):
+    with open(connection.QUESTION_FILE_PATH, newline='') as file:
+        reader = csv.DictReader(file)
+        for record in reader:
+            if record['id'] == question_id:
+                return record
