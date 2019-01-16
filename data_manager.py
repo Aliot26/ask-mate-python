0ce7f972d9688a345c45bfb685ee7e047b518ca9
Aliot26 <olgakochanowa@gmail.com>
2019-01-16 16:10:33 +0100

    Refactoring get_answers functionality

--- data_manager.py
+++ data_manager.py
@@ -25,16 +25,19 @@ def add_one_answer(answer):
     connection.save_data_in_csvfile(connection.ANSWER_FILE_PATH, answer, connection.ANSWER_HEADER)
 
 
-def get_one_question(question_id):
-    all_data = connection.get_all_data(connection.QUESTION_FILE_PATH)
-    for record in all_data:
-        if record['id'] == question_id:
-            return record
+def get_question(question_id=None):
+    all_questions = connection.get_all_data(connection.QUESTION_FILE_PATH)
+    if question_id:
+        for question in all_questions:
+            if question['id'] == question_id:
+                return question
+    return all_questions
+
 
 def get_answers(question_id):
-    all_data = connection.get_all_data(connection.ANSWER_FILE_PATH)
-    answers = []
-    for record in all_data:
-        if record['question_id'] == question_id:
-            answers.append(record)
-            return answers
+    answers = connection.get_all_data(connection.ANSWER_FILE_PATH)
+    answers_to_question = []
+    for answer in answers:
+        if answer['question_id'] == question_id:
+            answers_to_question.append(answer)
+    return answers_to_question
