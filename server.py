9c747f0e6a0b46546392611a29f3937bb818c4c2
Kat <kat.m.borowska@gmail.com>
2019-01-16 10:49:29 +0100

    add @app.route list + import connection

--- server.py
+++ server.py
@@ -1,5 +1,12 @@
 from flask import Flask, render_template, request, redirect, url_for
 
-import data_handler
+import data_manager
+import connection
 
 app = Flask(__name__)
+
+@app.route('/list')
+def route_list():
+    all_questions = connection.get_all_data()
+
+    return render_template('list.html', all_questions=all_questions)
