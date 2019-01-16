6ed46fc15333130158978928e620e8797532711e
Aliot26 <olgakochanowa@gmail.com>
2019-01-16 21:31:44 +0100

    Delete unused string

--- util.py
+++ util.py
@@ -9,7 +9,7 @@ def generate_timestamp():
 def convert_timestamp(timestamp):
     date_time = datetime.fromtimestamp(int(timestamp))
     return date_time
-    # return time.strftime("%m/%d/%Y, %H:%M:%S", (time.gmtime(timestamp)))
+
 
 
 
