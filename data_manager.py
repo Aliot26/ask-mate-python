b140e71b0116beabbb54b8d00d08d81dff0410e0
Kat <kat.m.borowska@gmail.com>
2019-01-17 10:17:16 +0100

    fix write_to_csv function

--- data_manager.py
+++ data_manager.py
@@ -12,7 +12,7 @@ def get_next_id(list_of_dict):
 
 
 def add_one_question(question):
-    all_data = get_processed_data(connection.QUESTION_FILE_PATH)
+    all_data= get_processed_data(connection.QUESTION_FILE_PATH)
     question['id'] = get_next_id(all_data)
     question['submission_time'] = util.generate_timestamp()
     connection.save_data_in_csvfile(connection.QUESTION_FILE_PATH, question, connection.QUESTION_HEADER)
