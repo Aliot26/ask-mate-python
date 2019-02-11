8f6ad9e1b5adbed3ebe1dbbf6b28ee2f1a28250d
Aliot26 <olgakochanowa@gmail.com>
2019-02-11 21:31:05 +0100

    Deleted unused code

--- validation/form_validation.py
+++ validation/form_validation.py
@@ -4,18 +4,6 @@ import re
 app = Flask(__name__)
 
 
-def get_comment_from_form():
-    message = remove_parentheses(request.form.get('message'))
-    message = check_is_not_empty(message)
-    if message:
-        comment = {
-            'message': message
-        }
-        return comment
-    else:
-        return False
-
-
 def check_conditions_of_sorting(conditions):
     attribute_list = ['submission_time', 'title']
     order_list = ['asc', 'desc']
